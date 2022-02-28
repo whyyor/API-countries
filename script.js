19b92b7d5945c065609dc96783fb480978c50c23
whyyor <whyyor@gmail.com>
2022-02-28 12:58:28 +0530

    fix country

--- script.js
+++ script.js
@@ -119,3 +119,36 @@ const getCountryData = function (country, errorMsg = `Something went wrong`) {
 btn.addEventListener('click', function () {
     getCountryData('India');
 })
+
+const whereAmI = function (lat, lng) {
+    fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+        .then(res => {
+            if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+            return res.json();
+        })
+        .then(data => {
+            console.log(data);
+            console.log(`You are in ${data.city}, ${data.country}`);
+
+            return fetch(`https://restcountries.eu/rest/v2/name/${data.country}`);
+        })
+        .then(res => {
+            if (!res.ok) throw new Error(`Country not found (${res.status})`);
+
+            return res.json();
+        })
+        .then(data => renderCountry(data[0]))
+        .catch(err => console.error(`${err.message} 💥`));
+};
+whereAmI(52.508, 13.381);
+
+const lotteryPromise = new Promise(function (resolve, reject) {
+    //This new promise executes as soon as it's created
+    //It take these two params
+    if (Math.random() >= 0.5) {
+        resolve('You win money');
+        //the param passed in resolve is message
+    } else {
+        reject(new Error('You lost your money lmao loser!'));
+    }
+});
